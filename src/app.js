47ce2a97d2ed225cb6e7737ee4b5522e6f9aef4b
eryssouza <ery.souza98@hotmail.com>
2022-09-07 22:30:13 +0100

    netlify problems... vai filhao

--- src/app.js
+++ src/app.js
@@ -1,36 +1,14 @@
 const express = require('express');
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
 const serverless = require('serverless-http');
-
+const ejs = require('ejs');
 
 const app = express();
 
+app.set('views','src/views');
+app.set('view engine', 'ejs');
 
-app.engine ('ejs', ejsMate )
-app.set ('view engine', 'ejs');
-app.set('views', 'src/views');
-app.set('public', 'src/public');
-// mongoose.connect('mongodb://localhost:3000/portfolio');
-app.use(express.urlencoded ({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'')));
-app.use(morgan('tiny'))
-const router = express.Router();
-
-router.get('/portfolio', (req, res) => {
-    res.render('home')
+app.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
-
-
-// app.listen(9000, () =>{
-//     console.log("Listening on port 9000!")
-// })
-
-app.use(`/.netlify/functions/api`, router);
-
-module.exports = app;
 module.exports.handler = serverless(app);
