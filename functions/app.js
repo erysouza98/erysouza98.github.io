35d165e3c833cd36ca51f4287e6f614eab5b8c5f
eryssouza <ery.souza98@hotmail.com>
2022-09-08 00:08:33 +0100

    logic

---
+++ functions/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path')
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+app.engine ('ejs', ejsMate )
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+module.exports.handler = serverless(app);
+
---
+++ functions/views/seeds/certificates.js
@@ -0,0 +1,27 @@
+module.exports =[
+    {
+        course: "English Certificate - C1",
+        platform: "EF SET",
+        url: "https://1drv.ms/b/s!AqSztURE3KZmb-J0IDcl4tvnUZY"
+    },
+    {
+        course: "React + Redux",
+        platform:"Sololearn",
+        url:"https://1drv.ms/b/s!AqSztURE3KZmceDOLrS5UydXWLQ",   
+    },
+    {
+        course:"The Web Developer Bootcamp 2022",
+        platform:"Udemy",
+        url:"https://1drv.ms/b/s!AqSztURE3KZmcnchVaKms91xSqI"
+    },
+    { 
+        course:"React & TypeScript Chrome Extension",
+        platform:"Udemy",
+        url:""
+    },
+    { 
+        course:"Complete React Developer 2022 ( w/ Redux, Hooks and GraphQL)",
+        platform:"Udemy",
+        url:""
+    }
+]
