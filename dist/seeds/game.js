328922a5a891e82fc99e2b864266a7c962286287
eryssouza <ery.souza98@hotmail.com>
2022-09-12 23:58:13 +0100

    loader and message colors

---
+++ dist/seeds/game.js
@@ -0,0 +1,103 @@
+var playerScore = 0;
+var playerSelection = 0;
+var computerSelection = 0;
+var computerScore = 0;
+var winner = -1;
+function play(select) {
+    playerSelection = select;
+
+    computerSelection = Math.floor((Math.random() * (3 - 1 + 1))) + 1;
+
+// 1- rock
+// 2- Scissors
+// 3-paper
+
+// 0-draw
+// 1-player wins
+// 2- cpu wins
+
+//Rock choose:
+      if((playerSelection == 1) && (computerSelection == 1)) {
+     winner = 0;
+       }
+       else if((playerSelection == 1) && (computerSelection == 2)) {
+        winner = 1;
+       }
+       else if((playerSelection == 1) && (computerSelection == 3)) {
+        winner = 2;
+       }
+// Scissors Choose:
+       else if((playerSelection == 2) && (computerSelection == 1)) {
+        winner = 2;
+       }
+       else if((playerSelection == 2) && (computerSelection == 2)) {
+           winner = 0;
+        }
+       else if((playerSelection == 2) && (computerSelection == 3)) {
+           winner = 1;
+        }  
+// Paper choose:
+        else if((playerSelection == 3) && (computerSelection == 1)) {
+        winner = 1;
+        }
+        else if((playerSelection == 3) && (computerSelection == 2)) {
+       winner = 2;
+        }
+        else if((playerSelection == 3) && (computerSelection == 3)) {
+       winner = 0;
+        } 
+
+
+
+       document.getElementById("player-selection-1").classList
+      .remove("selected");
+      document.getElementById("player-selection-2").classList
+      .remove("selected");
+      document.getElementById("player-selection-3").classList
+      .remove("selected");
+       
+      document.getElementById("computer-selection-1").classList
+      .remove("selected");
+      document.getElementById("computer-selection-2").classList
+      .remove("selected");
+      document.getElementById("computer-selection-3").classList
+      .remove("selected")
+
+
+       document.getElementById("player-selection-" + playerSelection).classList
+       .add("selected");
+       document.getElementById("computer-selection-" + computerSelection).classList
+       .add("selected");
+
+        if(winner == 0){
+        document.getElementById('message').innerHTML = 'Oh no, a draw... Try again.';
+        document.getElementById('message').classList.add("b-yellow")
+        document.getElementById('message').classList.remove("b-green")
+        document.getElementById('message').classList.remove("b-red")
+    }
+        else if(winner == 1){
+        document.getElementById('message').innerHTML = 'Congratulations!! You Won!!!';
+        document.getElementById('message').classList.add("b-green")
+        document.getElementById('message').classList.remove("b-yellow")
+        document.getElementById('message').classList.remove("b-red")
+        playerScore++;
+    }
+        else if(winner == 2){
+        document.getElementById('message').innerHTML = 'I won this one, but keep trying';
+        document.getElementById('message').classList.add("b-red")
+        document.getElementById('message').classList.remove("b-green")
+        document.getElementById('message').classList.remove("b-yellow");
+        computerScore++;
+    }
+
+
+    document.getElementById('player-score').innerHTML = playerScore;
+   
+    document.getElementById('computer-score').innerHTML = computerScore;
+
+    if(playerScore == 2 ){
+       document.getElementById("form").classList
+       .add("d-visible");
+   }
+    
+}  
--- functions/app.js
+++ functions/app.js
@@ -8,8 +8,7 @@ const app = express();
 app.engine ('ejs', ejsMate )
 
 app.set('views', path.join(__dirname, '/views'));
-app.use( express.static(path.join(__dirname,'/views/seeds')));
-app.use( express.static(path.join(__dirname,'/public')));
+
 // Todo Erika fix path das imagens.
 // app.use( express.static(path.join(__dirname,'/public/images')));
 app.set('view engine', 'ejs');
